d400c2cd8f167524100ca5bc01d476fd940f2214
James Capps <jcapps@protonmail.com>
2020-05-12 13:54:47 -0500

    Added enemies

--- app.cpp
+++ app.cpp
@@ -7,9 +7,6 @@
 #include <unistd.h>
 
 
-
-//Current issues.
-//Dealing negative damage and not stopping when health gets to zero.
 class Character {
     private: 
         int attack;
@@ -24,7 +21,6 @@ class Character {
         //another class.
         int health;
   
-
         Character(std::string name, int health,  int attack, int defense, 
                         int magic, int heal, int stealth) {
             this -> name = name;
@@ -55,6 +51,23 @@ class Character {
         // Add more functions here for other actions. 
 };
 
+class Weapon {
+    private:
+        int damage_amt;
+        int stability;
+
+    public:
+        std::string type;
+    
+    Weapon(std::string type, int damage_amt, int stability) {
+        this -> type = type;
+        this -> damage_amt = damage_amt;
+        this -> stability = stability;
+    }
+
+
+};
+
 class Battle {
     public:
         static void Initiate(Character& character1, Character& character2) {
@@ -83,10 +96,10 @@ class Battle {
             //Add more here later...
             
             //Health and Damage
-            int char1_damage = ceil(char2_attack - char1_defense);
+            int char1_damage = abs(ceil(char2_attack - char1_defense));
             character1.health = character1.health - char1_damage;
 
-            int char2_damage = ceil(char1_attack - char2_defense);
+            int char2_damage = abs(ceil(char1_attack - char2_defense));
             character2.health = character2.health -char2_damage; 
 
             printf("%s attacks %s and deals %d damage\n",
@@ -94,22 +107,14 @@ class Battle {
                     character2.name.c_str(),
                     char2_damage); 
 
-            usleep(20000);
+            usleep(2000000);
              
             printf("%s attacks %s and deals %d damage\n",
                     character2.name.c_str(),
                     character1.name.c_str(),
                     char1_damage);
 
-            usleep(200000);
-
-            printf("%s health: %d\n %s health: %d\n",
-                    character1.name.c_str(),
-                    character1.health,
-                    character2.name.c_str(),
-                    character2.health);
-            
-            usleep(200000);
+            usleep(2000000);
 
             if (character1.health < 0) {
                 character1.health = 0;
@@ -119,36 +124,117 @@ class Battle {
                 character2.health = 0;
             }
 
+
             if (character1.health == 0) {
-                printf("%s has died!\n %s WINS the battle!",
+                printf("%s has died!\n%s WINS the battle!",
                         character1.name.c_str(),
                         character2.name.c_str());
                 return "End of battle!";
-            } else {
-                return "rematch!!";
-            }
-
-            if (character2.health == 0) {
-                printf("%s has died\n %s WINS the battle!!",
+            } else if (character2.health == 0) {
+                printf("%s has died\n%s WINS the battle!!",
                         character2.name.c_str(),
                         character1.name.c_str());
                 
                 return "End of battle!";
-            } else {
-                return "rematch!!";
+            } else { 
+                printf("\t%s health: %d\n\t%s health: %d\n",
+                        character1.name.c_str(),
+                        character1.health,
+                        character2.name.c_str(),
+                        character2.health);
             }
 
+            return "End of turn!";
         }
 };
+/*
+void CharacterSelection() { 
+    int selection;
+    std::cout << "What character would you like to be?:\n(1) Warrior\n(2) Tank\n(3) Wizard\n(4) Healer\n(5) Thief\n";
+    std::cin >> selection;
+
+    switch(selection) {
+        case 1:
+            // name, health, attack, defense, magic, heal, stealth
+            std::cout << "You are a warrior!\n";
+            Character one("Warrior", 100, 10, 5, 0, 0, 0);
+            break;
+        case 2:
+            std::cout << "You are a tank!\n";
+           // return Character one("Tank", 100, 5, 10, 0, 0, 0);
+            break;
+        case 3: 
+            std::cout << "You are a wizard!\n";
+            //return Character one("Wizard", 100, 0, 5, 10, 0, 0);
+            break;
+        case 4:
+            std::cout << "You are a healer!\n";
+            //return Characterone("Healer", 100, 0, 0, 5, 10, 5);
+            break;
+        case 5:
+            std::cout << "You are a thief!\n";
+            //return Character one("Thief", 100, 3, 3, 0, 0, 10);
+            break;
+        default:
+            std::cout << "Please make a valid selection!\n";
+            CharacterSelection();
+    }
+}*/
+
+std::string RandomEnemy() {
+    int temp;
+    temp =std::rand() % 5;
+    std::vector<std::string> enemies = {"Demon", "Orc", "Giant Spider", "Troll", "Dragon"};
+    std::string enemy = enemies[temp];
+
+    return enemy;
+
+ /*   switch(enemy) {
+        case "Demon":
+            std::cout "You encounter a demon!\n";
+            return Character two("Demon", 200, 15, 5, 10, 0, 0);
+            break;
+        case "Orc":
+            std::cout "You encounter an Orc!\n";
+            return Chracter two("Orc", 50, 5, 2, 2, 0, 0);
+            break;
+        case "Giant Spider":
+            std::cout "You encounter a Giant Spider!\n";
+            return Character two("Giant Spider", 100, 10, 5, 0, 0, 0);
+            break;
+        case "Troll":
+            std::cout "You encounter a Troll!\n";
+            return Character two("Troll", 30, 3, 3, 0, 0, 0 );
+            break;
+        case "Dragon":
+            std::cout "You encounter a Dragon!\n";
+            return Character two("Dragon", 250, 20, 7, 5, 0, 0);
+            break;
+        default:
+            std::cout << "Something went wrong..." <<std::endl;
+            RandomEnemy();
+    }*/
+}
 
 int main() 
 {
     srand(time(NULL));
-         
+    /*     
     Character one("Warrior", 100, 100, 50, 25, 15, 20);
     Character two("Tank", 200, 30, 175, 10, 30,10);
+    
 
     Battle::Initiate(one, two);
+*/
+   // CharacterSelection();
+    std::string d;
+    d = RandomEnemy();
+
+    std::cout << d << std::endl;
+
+   // std::cout << CharacterSelection();
+   // Battle::Initiate(CharacterSelection(), RandomEnemy());
 
     return 0;
 }
+
