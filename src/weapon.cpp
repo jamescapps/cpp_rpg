d0420a1a0d6021344d882c0e6da443b9a0330b69
James Capps <jcapps@protonmail.com>
2020-05-18 19:07:31 -0500

    Removed all files that are in the gitignore

--- src/weapon.cpp
+++
@@ -1,8 +0,0 @@
-#include "../include/weapon.h"
-
-
-Weapon::Weapon(std::string type, int damage_amt, int stability) {
-    this -> type = type;
-    this -> damage_amt = damage_amt;
-    this -> stability = stability;
-}
